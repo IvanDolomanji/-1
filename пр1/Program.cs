e28d055477564af6b87b91e65e1099a9829de12e
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-11-24 20:28:33 +0200

    Add files via upload

---
+++ пр1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Console.WriteLine("     #######     ");
+            //Console.WriteLine("           #       ");
+            //Console.WriteLine("           #       ");
+            //Console.WriteLine("           #       ");
+            //Console.WriteLine("     #######     ");
+            //Console.WriteLine("           #       ");
+            //Console.WriteLine("           #       ");
+            //Console.WriteLine("           #       ");
+            //Console.WriteLine("     #######     ");
+            //Console.Read();
+
+            //2
+            Console.WriteLine("#         ");
+            Console.WriteLine("#         ");
+            Console.WriteLine("#         ");
+            Console.WriteLine("#         ");
+            Console.WriteLine("#         ");
+            Console.WriteLine("###################");
+            Console.WriteLine("#         ");
+            Console.WriteLine("#         ");
+            Console.WriteLine("#         ");
+            Console.WriteLine("#         ");
+            Console.WriteLine("#         ");
+            Console.Read();
+        }
+    }
+}
